e488b42f1171a1919dd05fccd1eefe8db26b3dc7
KaushikKC <kccreations1704@gmail.com>
2022-06-24 18:57:12 +0530

    Completed Header

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
+import { getServerSession } from 'next-auth'
+import { getSession } from "next-auth/react"
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home({session}) {
+  if (!session) return <Login />;
   return (
     <div >
       <Head>
         <title>Facebook</title>
       </Head>
       <Header />
+
+      <main>
+
+      </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session, 
+    }
+  }
 }
