8140ff99c983aa0489270c7c27621613a2f6bfef
KaushikKC <kccreations1704@gmail.com>
2022-06-24 18:57:37 +0530

    Completed Header

---
+++ components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Login() {
+  return (
+    <div>
+        <Image 
+            src="https://links.papareact.com/5me" 
+            width={400} 
+            height={400} 
+            objectFit="contain"
+        />
+    </div>
+  )
+}
+
+export default Login
